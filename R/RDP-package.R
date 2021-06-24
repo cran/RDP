c7d87bddd3cdd1558b1b1bc25c7e6ac43a551a5d
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-06-24 06:40:14 +0000

    version 0.1.4

---
+++ R/RDP-package.R
@@ -0,0 +1,18 @@
+#' `RDP`  package
+#'
+#' Implementation of the [Ramer-Douglas-Peucker algorithm](https://en.wikipedia.org/wiki/Ramer-Douglas-Peucker_algorithm).
+#'
+#' @docType package
+#' @name RDP-package
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib RDP, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
